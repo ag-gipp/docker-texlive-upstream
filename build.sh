48df59ecb1a27a9e505b35c523fa3ee127e840e0
adin <adin@ic.unicamp.br>
2018-02-13 15:09:50 -0200

    Add more verbose build messages

--- build.sh
+++ build.sh
@@ -7,21 +7,26 @@ fi
 BUILD_IT="false"
 
 if git diff $LAST_COMMIT HEAD --name-only | grep "$IMAGE_TAG.Dockerfile"; then
+  echo "Building $IMAGE_TAG since it changed from commit $LAST_COMMIT"
+  BUILD_IT="true"
+elif [ "$REBUILD_ALL" = "true" ]; then
+  echo "Building $IMAGE_TAG from rebuild all request"
+  BUILD_IT="true"
+elif [ "$REBUILD_STAGE" = "$IMAGE_TAG" ]; then
+  echo "Building $IMAGE_TAG from rebuild image stage request"
   BUILD_IT="true"
 else
-  if [ "$REBUILD_ALL" = "true" -o "$REBUILD_STAGE" = "$IMAGE_TAG" ]; then
-    BUILD_IT="true"
-  else
-    echo "$IMAGE_TAG up to date, not building"
-    echo "REBUILD_ALL=$REBUILD" >> variables
-  fi
+  echo "$IMAGE_TAG up to date, not building it"
+  echo "REBUILD_ALL=$REBUILD" >> variables
 fi
 
 if [ "$BUILD_IT" = "true" ]; then
   docker build --pull -t "$CI_REGISTRY_IMAGE:$IMAGE_TAG" -f "$IMAGE_TAG.Dockerfile" .
   docker push "$CI_REGISTRY_IMAGE:$IMAGE_TAG"
+  echo "Marking all future stages for rebuild"
   echo "REBUILD_ALL=true" >> variables
   if [ "$LATEST" = "$IMAGE_TAG" ]; then
+    echo "Pushing $IMAGE_TAG as latest too"
     docker tag "$CI_REGISTRY_IMAGE:$IMAGE_TAG" "$CI_REGISTRY_IMAGE:latest"
   fi
 fi

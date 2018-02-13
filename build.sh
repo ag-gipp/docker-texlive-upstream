3d2ee9700cb6dd6a67f5f95e0ac512771160ccf8
adin <adin@ic.unicamp.br>
2018-02-13 14:37:20 -0200

    Add support to tag latest image based on variable

--- build.sh
+++ build.sh
@@ -4,17 +4,24 @@ if [ -e "./variables" ]; then
   source ./variables
 fi
 
+BUILD_IT="false"
+
 if git diff $LAST_COMMIT HEAD --name-only | grep *.$IMAGE_TAG; then
-  docker build --pull -t "$CI_REGISTRY_IMAGE":$IMAGE_TAG -f Dockerfile.$IMAGE_TAG .
-  docker push "$CI_REGISTRY_IMAGE":$IMAGE_TAG
-  echo "REBUILD_ALL=true" >> variables
+  BUILD_IT="true"
 else
   if [ "$REBUILD_ALL" = "true" -o "$REBUILD_STAGE" = "$IMAGE_TAG" ]; then
-    docker build --pull -t "$CI_REGISTRY_IMAGE":$IMAGE_TAG -f Dockerfile.$IMAGE_TAG .
-    docker push "$CI_REGISTRY_IMAGE":$IMAGE_TAG
-    echo "REBUILD_ALL=true" >> variables
+    BUILD_IT="true"
   else
     echo "$IMAGE_TAG up to date, not building"
     echo "REBUILD_ALL=$REBUILD" >> variables
+  fi
+fi
+
+if [ "$BUILD_IT" = "true" ]; then
+  docker build --pull -t "$CI_REGISTRY_IMAGE:$IMAGE_TAG" -f "Dockerfile.$IMAGE_TAG" .
+  docker push "$CI_REGISTRY_IMAGE:$IMAGE_TAG"
+  echo "REBUILD_ALL=true" >> variables
+  if [ "$LATEST" = "$IMAGE_TAG" ]; then
+    docker tag "$CI_REGISTRY_IMAGE:$IMAGE_TAG" "$CI_REGISTRY_IMAGE:latest"
   fi
 fi

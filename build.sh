4d2b9a33dd3108005d3967e99157390d35819396
adin <adin@ic.unicamp.br>
2018-02-13 14:30:49 -0200

    Add rebuild stage, and moved sourced variables into script

--- build.sh
+++ build.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
+if [ -e "./variables" ]; then
+  source ./variables
+fi
+
 if git diff $LAST_COMMIT HEAD --name-only | grep *.$IMAGE_TAG; then
   docker build --pull -t "$CI_REGISTRY_IMAGE":$IMAGE_TAG -f Dockerfile.$IMAGE_TAG .
   docker push "$CI_REGISTRY_IMAGE":$IMAGE_TAG
-  echo "REBUILD=true" >> variables
+  echo "REBUILD_ALL=true" >> variables
 else
-  if [ "$REBUILD" = "true" ]; then
+  if [ "$REBUILD_ALL" = "true" -o "$REBUILD_STAGE" = "$IMAGE_TAG" ]; then
     docker build --pull -t "$CI_REGISTRY_IMAGE":$IMAGE_TAG -f Dockerfile.$IMAGE_TAG .
     docker push "$CI_REGISTRY_IMAGE":$IMAGE_TAG
-    echo "REBUILD=true" >> variables
+    echo "REBUILD_ALL=true" >> variables
   else
     echo "$IMAGE_TAG up to date, not building"
-    echo "REBUILD=$REBUILD" >> variables
+    echo "REBUILD_ALL=$REBUILD" >> variables
   fi
 fi
